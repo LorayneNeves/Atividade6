7bf88d76bf87ff16a56c33a5302661391c932f9c
Lorayne Neves <lorayne3.8neves@outlook.com>
2023-04-28 21:45:40 -0300

    Adicionar arquivos de projeto.

---
+++ AnalisarVirusCOVID_H1N1/Covid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisarVriusCOVID_H1N1
+{
+    public class Covid : Virus
+    {
+        public int TotalQuarentenaDias { get; set; }
+        public DateTime data;
+
+
+        public DateTime CalcularDataRetorno()
+        {
+            data = DateTime.Today.AddDays(TotalQuarentenaDias);
+            return data;
+        }
+        public override void ImprimirRecomendacoesTratamento()
+        {
+            Console.WriteLine("Voce devera retornar em:" + data);
+        }
+    }
+}
---
+++ AnalisarVirusCOVID_H1N1/Virus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisarVriusCOVID_H1N1
+{
+    public abstract class Virus
+    {
+        public string nome { get; set; }
+        public string Observacao { get; set; }
+
+        public abstract void ImprimirRecomendacoesTratamento();
+
+
+
+    }
+}
