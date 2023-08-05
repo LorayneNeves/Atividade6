f9d4bbe50970b36c3f4f2317a9e2b6eea250d6dd
LorayneNeves <lorayne3.8neves@outlook.com>
2023-08-04 21:50:33 -0300

    Adicionar arquivos de projeto.

---
+++ listaTarefas/tarefas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaTarefas
+{
+    public class tarefas
+    {
+        public string nome;
+        public int esforco; // em horas
+        public bool finalizado = false;
+
+
+        public void gerarTarefa(string Nome, int Esforco)
+        {
+            nome = Nome;
+            esforco = Esforco;
+        }
+
+        public bool tarefafeita(bool finalizado)
+        {
+
+            bool final = finalizado;
+
+
+
+            return finalizado;
+        }
+    }
+}
